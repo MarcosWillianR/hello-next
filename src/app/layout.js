eec78d2d7d285153391e314000161a4004474640
portfolioflex <portfolioflex2023@gmail.com>
2024-04-09 21:22:15 -0300

    feat: finish exercise

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,11 +3,13 @@ import React from 'react';
 import './styles.css';
 
 function RootLayout({ children }) {
+  console.log('layout rendered')
+
   return (
     <html lang="en">
       <body>
         {children}
-
+        <footer>Page rendered on {new Date().toLocaleString()}</footer>
       </body>
     </html>
   );
